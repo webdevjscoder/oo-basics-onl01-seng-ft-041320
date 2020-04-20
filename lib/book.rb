b7ef2b06572e0675284f987fa65b145a9b5475b4
Juan Cobian <cobianjuan19@gmail.com>
2020-04-20 18:18:48 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -14,6 +14,14 @@ class Book
   def author
     @author
   end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end 
 end
 
 Book.new("And Then There Were None")
